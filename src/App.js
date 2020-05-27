e704758a159a80b1b42e01394386680a21edc08c
Walmyr <wlsf82@gmail.com>
2020-05-27 03:27:19 +0200

    Refactor components to use arrow function

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import React from 'react';
 
 import {List} from './components/List';
 
-function App() {
-  return (
-    <div>
-      <h1>My Hacker Stories</h1>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text"></input>
-      <hr />
-      <List />
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <h1>My Hacker Stories</h1>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text"></input>
+    <hr />
+    <List />
+  </div>
+);
 
 export default App;
--- src/components/List.js
+++ src/components/List.js
@@ -19,17 +19,15 @@ const list = [
   },
 ];
 
-export function List() {
-  return list.map(item => {
-    return (
-      <div key={item.objectID}>
-        <span>
-          <a href={item.url}>{item.title}</a>
-        </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-      </div>
-    );
-  })
-}
+export const List = () =>
+  list.map(item => (
+    <div key={item.objectID}>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+    </div>
+  )
+);
