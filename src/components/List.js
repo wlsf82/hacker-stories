5da0d83f87ad933ea96eb46e242aaed496157749
Walmyr <wlsf82@gmail.com>
2020-06-01 18:52:20 +0200

    Use item component inside list component

--- src/components/List.js
+++ src/components/List.js
@@ -1,13 +1,6 @@
 import React from 'react';
 
-export const List = props =>
-  props.list.map(item =>
-    <div key={item.objectID}>
-      <span>
-        <a href={item.url}>{item.title}</a>
-      </span>
-      <span>{item.author}</span>
-      <span>{item.num_comments}</span>
-      <span>{item.points}</span>
-    </div>
-  )
+import { Item } from './Item'
+
+export const List = ({ list }) =>
+  list.map(item => <Item key={item.objectID}  item={item} />)
