fdb0c82c319f31f69aab7c4ec6e0075abb5b75c1
Walmyr <wlsf82@gmail.com>
2020-06-01 18:52:02 +0200

    Create item component

---
+++ src/components/Item.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Item = ({ item }) =>
+  <div>
+    <span>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
+  </div>
