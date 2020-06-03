14e6215a92cfdbd8bc0f94a8c4920d326bdb0bd9
Walmyr <wlsf82@gmail.com>
2020-06-03 23:11:56 +0200

    Better separate responsibilities
    Before, the searchTerm was used for two cases: updating the input
    field’s state and activating the side-effect for fetching data.
    
    Now it’s only used for the former. A second state called url got
    introduced for triggering the side-effect for fetching data which
    only happens when a user clicks the confirmation button.

--- src/App.js
+++ src/App.js
@@ -15,17 +15,27 @@ const App = () => {
     'React'
   );
 
+  const [url, setUrl] = React.useState(
+    `${API_ENDPOINT}${searchTerm}`
+  );
+
   const [stories, dispatchStories] = React.useReducer(
     storiesReducer,
     { data: [], isLoading: false, isError: false }
   );
 
-  const handleFetchStories = React.useCallback(() => {
-    if (!searchTerm) return;
+  const handleSearchInput = event => {
+    setSearchTerm(event.target.value);
+  };
 
+  const handleSearchSubmit = () => {
+    setUrl(`${API_ENDPOINT}${searchTerm}`);
+  };
+
+  const handleFetchStories = React.useCallback(() => {
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-    fetch(`${API_ENDPOINT}${searchTerm}`)
+    fetch(url)
       .then(response => response.json())
       .then(result => {
         dispatchStories({
@@ -36,7 +46,7 @@ const App = () => {
       .catch(() =>
         dispatchStories({ type: 'STORIES_FETCH_FAILURE'})
       );
-  }, [searchTerm]);
+  }, [url]);
 
   React.useEffect(() => {
     handleFetchStories();
@@ -49,10 +59,6 @@ const App = () => {
     });
   };
 
-  const handleSearch = event => {
-    setSearchTerm(event.target.value)
-  };
-
   return (
     <div>
       <h1>My Hacker Stories</h1>
@@ -61,10 +67,17 @@ const App = () => {
         id="search"
         value={searchTerm}
         isFocused
-        onInputChange={handleSearch}
+        onInputChange={handleSearchInput}
       >
         <strong>Search:</strong>
       </InputWithLabel>
+
+      <button
+        type="button"
+        disabled={!searchTerm}
+        onClick={handleSearchSubmit}>
+        Submit
+      </button>
 
       <hr />
 
