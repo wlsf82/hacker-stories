29f21dbc0c9276ad10bdcc44e52aacba56b1ec57
Walmyr <wlsf82@gmail.com>
2020-06-06 15:51:37 +0200

    Add three more tests for the search form component

--- src/tests/SearchForm.test.tsx
+++ src/tests/SearchForm.test.tsx
@@ -21,4 +21,28 @@ describe('SearchForm', () => {
 
     expect(value).toEqual(searchFormProps.searchTerm);
   });
+
+  it('changes the input field', () => {
+    const pseudoEvent = { target: 'Redux' };
+
+    component.root.findByType('input').props.onChange(pseudoEvent);
+
+    expect(searchFormProps.onSearchInput).toHaveBeenCalledTimes(1);
+    expect(searchFormProps.onSearchInput).toHaveBeenCalledWith(pseudoEvent);
+  });
+
+  it('submits the form', () => {
+    const pseudoEvent = {};
+
+    component.root.findByType('form').props.onSubmit(pseudoEvent);
+
+    expect(searchFormProps.onSearchSubmit).toHaveBeenCalledTimes(1);
+    expect(searchFormProps.onSearchSubmit).toHaveBeenCalledWith(pseudoEvent);
+  });
+
+  it('disables the button and prevents submit', () => {
+    component.update(<SearchForm {...searchFormProps} searchTerm="" />);
+
+    expect(component.root.findByType('button').props.disabled).toBeTruthy();
+  });
 });
